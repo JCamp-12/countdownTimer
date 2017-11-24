039635ef5d4e6e49bb57cf10b4b6b89501449bf7
JCamp-12 <username@users.noreply.github.com>
2017-11-23 22:51:17 -0500

    css setup

--- sideProject1/countdownTimer/src/App.jsx
+++ sideProject1/countdownTimer/src/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-    <div>Countdown Timer, App Component</div>
+    <div className="App">
+    <div className="App-title"> Countdown to Date </div>
+    <div>
+      <div className="Clock-days">14 Days</div>
+      <div className="Clock-hours">30 hours</div>
+      <div className="Clock-minutes">15 minutes</div>
+      <div className="Clock-seconds">20 seconds</div>
+    </div>
+      <input placeholder='new date'/>
+      <button>Submit</button>
+    </div>
     )
   }
 }
