533501e2afed3f6b6818e7739999b1d1c866ef0a
JCamp-12 <username@users.noreply.github.com>
2017-11-27 10:59:55 -0500

    good for now, will refactor later for more use of specific time and a stop watch

--- sideProject1/countdownTimer/src/App.jsx
+++ sideProject1/countdownTimer/src/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -18,19 +19,22 @@ class App extends Component {
   render() {
     return (
     <div className="App">
-    <div className="App-title">
-      Countdown to {this.state.deadline}
+      <div className="App-title">
+        Countdown to {this.state.deadline}
       </div>
-    <div>
       <Clock 
         deadline={this.state.deadline}
       />
-    </div>
-      <input 
-        placeholder='new date'
-        onChange={event => this.setState({newDeadline: event.target.value})}
+      <Form inline>
+        <FormControl
+          className= "Deadline-input"
+          placeholder='new date = mm/dd/xxxx'
+          onChange={event => this.setState({newDeadline: event.target.value})}
         />
-      <button className="button" onClick={() => this.changeDeadline()}>Submit</button>
+        <Button className="button" onClick={() => this.changeDeadline()}>
+          Submit
+        </Button>
+      </Form>
     </div>
     )
   }
